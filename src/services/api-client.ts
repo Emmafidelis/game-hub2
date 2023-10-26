bd85a530e737044e133bee75b18969cbe24fa32c
Emanuel Fidelis <125068974+Emmafidelis@users.noreply.github.com>
2023-10-26 15:14:58 +0300

    Initial commit

---
+++ src/services/api-client.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+  next: string | null;
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "34aedfa8e649450190988ede5ce518bd",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
